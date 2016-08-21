13d88d69088dd032424cbdbef9f27a95d6d17c02
Steven Blom <atomicblom@binaryvibrance.net>
2016-08-21 22:21:28 +0930

    setting up build script

--- src/main/java/com/github/atomicblom/shearmadness/utility/Reference.java
+++ src/main/java/com/github/atomicblom/shearmadness/utility/Reference.java
@@ -2,9 +2,9 @@ package com.github.atomicblom.shearmadness.utility;
 
 public class Reference
 {
-    public static final String MOD_ID = "shearmadness";
+    public static final String MOD_ID = "@MOD_ID@";
     public static final String MOD_NAME = "Chisel Sheep";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "@MOD_VERSION@";
     public static final String MOD_GUI_FACTORY = "com.github.atomicblom.shearmadness.configuration.client.ModGuiFactory";
     public static final String BEHAVIOUR_COMMENT = "Sets the behaviour when a sheep is sheared\n" +
             "* RevertSheep - will change the sheep back to a normal sheep (default).\n" +
