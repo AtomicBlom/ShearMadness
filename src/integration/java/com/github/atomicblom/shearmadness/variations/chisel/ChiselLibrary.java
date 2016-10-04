5f5735516c06163af48e5ecd3b49d2a20c56d04e
Steven Blom <atomicblom@binaryvibrance.net>
2016-10-04 21:39:08 +1100

    Fixing up Integrations

---
+++ src/integration/java/com/github/atomicblom/shearmadness/variations/chisel/ChiselLibrary.java
@@ -0,0 +1,15 @@
+package com.github.atomicblom.shearmadness.variations.chisel;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@SuppressWarnings("ALL")
+@ObjectHolder("chisel")
+public class ChiselLibrary
+{
+    public static final Block technical = null;
+    public static final Block technical1 = null;
+
+    public static final Block marbleextra = null;
+}
